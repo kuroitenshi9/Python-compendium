68feedb70d9dca185d30436253149862c6cda61c
unknown <ewabielawska@daum.net>
2019-03-10 12:41:44 +0100

    exercises from first class

---
+++ 1/1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+
+"""
+kalkulator BMI
+- na podstawie wzoru BMI w konsolu oblicz swoje BMI
+- spróbuj napisać rozwiązanie w jednej linii
+"""
+
+wzrost = 1.70
+waga = 59
+BMI = waga / (wzrost ** 2)
+
+print("Twoje BMI wynosi:",BMI)
+
+print("Twoje BMI wynosi:", 59/(1.70 ** 2))
+
